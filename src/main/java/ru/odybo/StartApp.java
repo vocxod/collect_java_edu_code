1a062c0bd7014f6614b625b2f7973763d5ead203
Serg Ryabinin <scanner85@gmail.com>
2022-07-23 15:04:54 +0300

    Clone simple App.java

---
+++ src/main/java/ru/odybo/StartApp.java
@@ -0,0 +1,13 @@
+package ru.odybo;
+
+/**
+ * Hello world!
+ *
+ */
+public class StartApp 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Start App!" );
+    }
+}
