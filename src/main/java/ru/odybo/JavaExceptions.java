a8c3c0890501e5a7cfc952151bb12c427e4ce592
Serg Ryabinin <scanner85@gmail.com>
2022-07-25 11:20:08 +0300

    Try resource exceptions

--- src/main/java/ru/odybo/JavaExceptions.java
+++ src/main/java/ru/odybo/JavaExceptions.java
@@ -26,12 +26,24 @@ public class JavaExceptions{
   public static void main(String[] args){
     System.out.println("Start app");
 
+    // this code dont compile
+    // Alternatives in a multi-catch statement cannot be related by subclassing
+    // In this example, ArithmeticException and ArrayIndexOutOfBoundsException are 
+    // both subclasses of the Exception class. So, we get a compilation error.
+    
     try {
       int array[] = new int[10];
       array[10] = 30 / 0;
-    } catch (Exception | ArithmeticException | ArrayIndexOutOfBoundsException e) {
+    // Dont it! (bellow)
+    // } catch (ArrayIndexOutOfBoundsException | ArithmeticException | Exception  e) {
+    } catch (ArrayIndexOutOfBoundsException | ArithmeticException e) {
+      System.out.println("Only special exceptions");
+      System.out.println(e.getMessage());
+    } catch (Exception e){
+      System.out.println("All exceptions");
       System.out.println(e.getMessage());
     }
+    
 
     ListOfNumbers list = new ListOfNumbers();
     list.writeList();
